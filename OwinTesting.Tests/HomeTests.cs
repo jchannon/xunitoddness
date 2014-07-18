a369b30a07278781cd28b0cec473c4ba81089762
Jonathan Channon <jonathan.channon@gmail.com>
2014-07-18 10:19:28 +0100

    initial

---
+++ OwinTesting.Tests/HomeTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinTesting.Tests
+{
+    using System.Net;
+    using System.Net.Http;
+    using Microsoft.Owin.Testing;
+    using Xunit;
+
+    public class HomeTests
+    {
+        [Fact]
+        public async Task DoesItWork()
+        {
+            //Given
+            var client = CreateHttpClient();
+
+            //When
+            var response = await client.GetAsync("http://localhost/");
+
+            //Then
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            
+        }
+
+        private static HttpClient CreateHttpClient()
+        {
+            var client = TestServer.Create(builder =>
+                new Startup()
+                .Configuration(builder))
+                .HttpClient;
+
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
+        }
+    }
+}
---
+++ OwinTesting/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace OwinTesting
+{
+    using System.Diagnostics;
+    using Nancy;
+    using Nancy.Bootstrapper;
+    using Nancy.TinyIoc;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy(options => options.Bootstrapper = new MyBootstrapper());
+        }
+    }
+
+    public class MyBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+            Debug.WriteLine("Hi there");
+        }
+    }
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Boo";
+        }
+    }
+}
